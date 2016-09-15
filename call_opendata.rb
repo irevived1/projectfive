56ebdc258d2cda7df115978b02d078179ab011ba
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-09-15 17:59:39 -0400

    CSV files generated

--- call_opendata.rb
+++ call_opendata.rb
@@ -1,15 +1,48 @@
 require 'csv'
 require 'byebug'
 
-tree_hash = {}
+noise_hash = Hash.new(0)
+light_hash = Hash.new(0)
+heat_hash = Hash.new(0)
+graffiti_hash = Hash.new(0)
+parking_hash = Hash.new(0)
 
-CSV.foreach('trees_by_zip.csv', headers: true) do |tree|
-  tree_hash[tree[2]] ? tree_hash[tree[2]] += 1 : tree_hash[tree[2]] = 1
+CSV.foreach('complaints_min.csv', headers: true) do |complaint|
+  case complaint[0]
+  # when "Noise - Residential"
+  #   noise_hash[complaint[1]] += 1
+  when "HEATING"
+    heat_hash[complaint[1]] += 1
+  when "Street Light Condition"
+    light_hash[complaint[1]] += 1
+  when "Graffiti"
+    graffiti_hash[complaint[1]] += 1
+  when "Illegal Parking"
+    parking_hash[complaint[1]] += 1
+  end
 end
 
-CSV.open('public/trees.csv', 'wb') do |csv|
-  csv << ['zip', 'count']
-  tree_hash.each do |zip, count|
+CSV.open('public/heating_complaints.csv', 'a+') do |csv|
+  heat_hash.each do |zip, count|
+    csv << [zip, count]
+  end
+end
+
+CSV.open('public/streetlight_complaints.csv', 'a+') do |csv|
+  light_hash.each do |zip, count|
+    csv << [zip, count]
+  end
+end
+
+CSV.open('public/graffiti_complaints.csv', 'a+') do |csv|
+  graffiti_hash.each do |zip, count|
+    csv << [zip, count]
+  end
+end
+
+
+CSV.open('public/illegal_parking.csv', 'a+') do |csv|
+  parking_hash.each do |zip, count|
     csv << [zip, count]
   end
 end
