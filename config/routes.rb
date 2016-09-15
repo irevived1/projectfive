c4d1229c76de761f2f16970e9fa5da8478d7406d
GordonNY <gordonsdev@gmail.com>
2016-09-15 17:38:57 -0400

    add display action route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root to: "about#index"
+  post 'display' => 'home#display'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
