fd241c9a9877a544f7457e9fb6339c3678aa22c2
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-16 18:29:31 -0400

    graph can be sorted

--- app/assets/javascripts/input.js
+++ app/assets/javascripts/input.js
@@ -29,77 +29,83 @@ function graphChanger(input) {
 
 $(function () {
   
-$('form button').click(function(e) {
-d3.select("svg").remove();
-d3.csv("/assets/master.csv", function(error, data) {
-  if (error) throw error;
+  $('form button').click(function(e) {
+  // d3.select("svg").remove();
+  $('body div#graphy').prepend("<p></p>");
+  var firstptag = $('body div#graphy p:first-child')
+  d3.csv("/assets/master.csv", function(error, data) {
+    if (error) throw error;
 
-  var tmp = Object.keys(data[0])
-  var zipcode = tmp[0]
-  var gname = graphChanger($('form option:selected').html());
-  var value=tmp[gname == undefined ? 1 : gname];
-  var name_of_column = value.replace(/_/g," ");
-  $('#insertyourtitlehere').html(name_of_column.toUpperCase());
-  $('#zipcode').html(tmp[0].toUpperCase()).css('transform', 'rotate(90deg)');
+    var tmp = Object.keys(data[0])
+    var zipcode = tmp[0]
+    var gname = graphChanger($('select[name="fname"]').val());
+    var value=tmp[gname == undefined ? 1 : gname];
+    var name_of_column = value.replace(/_/g," ");
+    var typeofsort = $('select[name="typeofsort"]').val()
+    firstptag.append('<h2 class="graphytitle">' + name_of_column.toUpperCase() + ' SORT BY ' + typeofsort.toUpperCase() + '</h3>');
+    firstptag.append('<h3>' + tmp[0].toUpperCase() + '</h3>');
 
-  var m = [60, 10, 10, 60],
-      w = 800 - m[1] - m[3],
-      h = (15*data.length) - m[0] - m[2];
+    var m = [60, 10, 10, 60],
+        w = 800 - m[1] - m[3],
+        h = (15*data.length) - m[0] - m[2];
 
-  var format = d3.format(",.0f");
+    var format = d3.format(",.0f");
 
-  var x = d3.scale.linear().range([0, w]),
-      y = d3.scale.ordinal().rangeRoundBands([0, h], .1);
+    var x = d3.scale.linear().range([0, w]),
+        y = d3.scale.ordinal().rangeRoundBands([0, h], .1);
 
-  var xAxis = d3.svg.axis().scale(x).orient("top").tickSize(-h),
-      yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
+    var xAxis = d3.svg.axis().scale(x).orient("top").tickSize(-h),
+        yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
 
-  var svg = d3.select("body").append("svg")
-      .attr("width", w + m[1] + m[3])
-      .attr("height", h + m[0] + m[2])
-    .append("g")
-      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+    var svg = d3.select("body div#graphy p:first-child").append("svg")
+        .attr("width", w + m[1] + m[3])
+        .attr("height", h + m[0] + m[2])
+      .append("g")
+        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
 
-  // Parse numbers, and sort by SCORE.
-  data.forEach(function(d) { d[value] = +d[value]; });
-  //data.sort(function(a, b) { return b.SCORE - a.SCORE; });
-  data.sort(function(a, b) { return b[zipcode] - a[zipcode]; });
+    // Parse numbers, and sort by SCORE.
+    data.forEach(function(d) { d[value] = +d[value]; });
+    if ( typeofsort == "value" ) {
+      data.sort(function(a, b) { return b[value] - a[value]; });
+    } else {
+      data.sort(function(a, b) { return b[zipcode] - a[zipcode]; });
+    }
 
-  // Set the scale domain.
-  x.domain([0, d3.max(data, function(d) { return d[value]; })]);
-  y.domain(data.map(function(d) { return d[zipcode]; }));
-  var max = d3.max(data, function(d) { return d[value]; });
+    // Set the scale domain.
+    x.domain([0, d3.max(data, function(d) { return d[value]; })]);
+    y.domain(data.map(function(d) { return d[zipcode]; }));
+    var max = d3.max(data, function(d) { return d[value]; });
 
-  var bar = svg.selectAll("g.bar")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) { return "translate(0," + y(d[zipcode]) + ")"; });
+    var bar = svg.selectAll("g.bar")
+        .data(data)
+      .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(0," + y(d[zipcode]) + ")"; });
 
-  bar.append("rect")
-      .attr("width", function(d) { return x(d[value]); })
-      .attr("height", y.rangeBand());
+    bar.append("rect")
+        .attr("width", function(d) { return x(d[value]); })
+        .attr("height", y.rangeBand());
 
-  bar.append("text")
-      .attr("class", "value")
-      .attr("x", function(d) { return (x(d[value]) +  (shiftvalue((d[value]),max))   ); })
-      .attr("y", y.rangeBand() / 2)
-  //shiftvalue(x(d[value]))
-      .attr("dx", -3 )
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .text(function(d) { return format(d[value]); });
+    bar.append("text")
+        .attr("class", "value")
+        .attr("x", function(d) { return (x(d[value]) +  (shiftvalue((d[value]),max))   ); })
+        .attr("y", y.rangeBand() / 2)
+    //shiftvalue(x(d[value]))
+        .attr("dx", -3 )
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .text(function(d) { return format(d[value]); });
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .call(xAxis);
+    svg.append("g")
+        .attr("class", "x axis")
+        .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-});
-e.preventDefault();
-});
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+  });
+  e.preventDefault();
+  });
 });
 // $(document).ready(function() {
 //   window.restaurant_average_score = {};
