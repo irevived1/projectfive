523363f2248bc2ac8c81374d641f1852f51267b0
GordonNY <gordonsdev@gmail.com>
2016-09-16 16:58:30 -0400

    test unipre function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,6 +17,7 @@ class HomeController < ApplicationController
 		up = UniPre.new(x, y)
 		up.n
 		
+    @theta = up.theta
 
     # term1 = (Object.const_get params[:term1]).all
     # term2 = (Object.const_get params[:term2]).all
