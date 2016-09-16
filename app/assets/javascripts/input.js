2e060b8d53dee5902ccfb50f8cace916f452e7b4
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-16 13:51:09 -0400

    added dynamic stuff

--- app/assets/javascripts/input.js
+++ app/assets/javascripts/input.js
@@ -1,18 +1,28 @@
-$(document).ready(function() {
-  window.restaurant_average_score = {};
-  $.ajax({
-      type: "GET",
-      url: "assets/restaurant_average_score.csv",
-      dataType: "text",
-      success: function(data) {processData(data);}
-   });
-});
-
-
-function processData(allText) {
-  var arr = allText.split("\n");
-  for (var i = 0, len = arr.length; i < len; i++) {
-    var morearr = arr[i].split(",");
-    window.restaurant_average_score[morearr[0]] = parseInt(morearr[1]);
-  }
+function shiftvalue(input) {
+  var length = count_digit(input);
+  length = length + Math.floor((length-1)/3);
+  return length;
 }
+
+function count_digit(input) {
+  return String(input).length;
+}
+
+// $(document).ready(function() {
+//   window.restaurant_average_score = {};
+//   $.ajax({
+//       type: "GET",
+//       url: "assets/restaurant_average_score.csv",
+//       dataType: "text",
+//       success: function(data) {processData(data);}
+//    });
+// });
+
+
+// function processData(allText) {
+//   var arr = allText.split("\n");
+//   for (var i = 0, len = arr.length; i < len; i++) {
+//     var morearr = arr[i].split(",");
+//     window.restaurant_average_score[morearr[0]] = parseInt(morearr[1]);
+//   }
+// }
