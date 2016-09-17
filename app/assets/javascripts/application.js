85da3ba7a62d03f0cfe2d360a625459e2d0950a4
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-16 21:52:08 -0400

    added d3 min to lib/assets/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require d3.v3.min
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w(input.js)
+Rails.application.config.assets.precompile += %w(input.js d3.v3.min.js)
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
