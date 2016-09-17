374bb022cca00a47946c1d63d581e15885715695
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-09-17 06:23:34 -0400

    making splash play nice with rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(input.js d3.v3.min.js)
+Rails.application.config.assets.precompile += %w( test.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
