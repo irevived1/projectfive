e735252b1317c0340a60f364391e253342dbe5bb
GordonNY <gordonsdev@gmail.com>
2016-09-18 18:25:19 -0400

    fix css conflicts after merged master

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,9 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(input.js d3.v3.min.js)
 Rails.application.config.assets.precompile += %w( test.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( abel.css )
+Rails.application.config.assets.precompile += %w( slippry.css )
+Rails.application.config.assets.precompile += %w( linechart.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
