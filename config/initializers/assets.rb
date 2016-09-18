4018762bd84dbcf5918ac14015eccbe163c270e6
GordonNY <gordonsdev@gmail.com>
2016-09-18 18:27:07 -0400

    add comment in initializer assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,14 +5,19 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(input.js d3.v3.min.js)
 Rails.application.config.assets.precompile += %w( test.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
+# For linechart initialization  -- begin
 Rails.application.config.assets.precompile += %w( abel.css )
 Rails.application.config.assets.precompile += %w( slippry.css )
 Rails.application.config.assets.precompile += %w( linechart.css )
+require "#{Rails.root}/lib/UniPre.rb"
+require "#{Rails.root}/lib/FileDataCache.rb"
+# -- end
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-require "#{Rails.root}/lib/UniPre.rb"
-require "#{Rails.root}/lib/FileDataCache.rb"
+
